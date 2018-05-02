8476527567d215aaa0d4cfdd936735f2f5842afb
xiaowei <weige0088@126.com>
2018-05-02 22:19:57 +0800

    解除绑定shopId

--- src/page/goodsAddNew.vue
+++ src/page/goodsAddNew.vue
@@ -479,7 +479,7 @@ export default {
       //创建提交data对象
       let data = Object.create(null);
 
-      data.shopId = '5' //商家id(写死的一个数值)
+      // data.shopId = '5' //商家id(写死的一个数值)
       data.productName = this.goodsName; //商品名称
       data.listPrice = this.goodsPrice; //商品价格
       data.mainImage = this.mainimageUrl; //商品默认主图链接
